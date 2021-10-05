8c9b248d27f59afc810c0ef2afe8641781a31b2a
Devrim Demir <33595208+devrimos@users.noreply.github.com>
2021-10-05 21:19:07 +0300

    Update nova.js

--- nova.js
+++ nova.js
@@ -1,2 +1,4 @@
 var bgColor=document.querySelectorAll("[data-bgColor]");bgColor.forEach(element=>{element.style.backgroundColor=element.getAttribute("data-bgColor");})
 var bgImg=document.querySelectorAll("[data-bgImg]");for(var i=0;i<bgImg.length;i++){var src=bgImg[i].getAttribute('data-bgImg');bgImg[i].style.backgroundImage="url('" + src + "')";}
+var bColor=document.querySelectorAll("[data-bColor]");bColor.forEach(element=>{element.style.borderColor=element.getAttribute("data-bColor");})
+var color=document.querySelectorAll("[data-color]");color.forEach(element=>{element.style.color=element.getAttribute("data-color");})
